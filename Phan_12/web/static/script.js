5666ff9670a52638a024ab7bdac82023ee59a3a4
Minh Nghia <deptrainghia5@gmail.com>
2025-06-04 08:37:28 +0700

    update

--- Phan_12/web/static/script.js
+++ Phan_12/web/static/script.js
@@ -6,12 +6,6 @@ document.getElementById('predictionForm').addEventListener('submit', async(e) =>
     const bathrooms = parseInt(document.getElementById('bathrooms').value);
     const stories = parseInt(document.getElementById('stories').value);
 
-    const data = {
-        area: area, 
-        bedrooms: bedrooms,
-        bathrooms: bathrooms,
-        stories: stories
-    }
 
     try{
         const response = await fetch('/predict', {
